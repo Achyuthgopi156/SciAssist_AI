143e796361be6784c662288cf2436100a07e2542
Gopi Raju Rajolu <72077967+Achyuthgopi156@users.noreply.github.com>
2025-05-06 20:50:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def get_conversation_chain(retriever):
     If you don't know the answer, just say that you don't know. \
     Use three sentences maximum and keep the answer concise.\
     
-    {context}"""
+    {context}
     
     qa_prompt = ChatPromptTemplate.from_messages(
         [
